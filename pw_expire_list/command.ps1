24226eb53d8356486cd2e6ebc2f8b55dc075af4f
jcarter <Running-97-miles>
2023-05-30 09:39:08 -0700

    Updated to include expired users.

--- pw_expire_list/command.ps1
+++ pw_expire_list/command.ps1
@@ -8,5 +8,30 @@ $MaxPwdAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days
 $expiredDate = (Get-Date).addDays(-$MaxPwdAge)
 #Set the number of days until you would like to begin notifing the users. -- Do Not Modify --
 #Filters for all users who's password is within $date of expiration.
-$ExpiredUsers = Get-ADUser -Filter {(PasswordLastSet -gt $expiredDate) -and (PasswordNeverExpires -eq $false) -and (Enabled -eq $true)} -Properties PasswordNeverExpires, PasswordLastSet, Mail | select samaccountname, PasswordLastSet, Mail, @{name = "DaysUntilExpired"; Expression = {$_.PasswordLastSet - $ExpiredDate | select -ExpandProperty Days}} | Sort-Object PasswordLastSet
-$ExpiredUsers | ConvertTo-Json
+$NonExpiredUsers = Get-ADUser -Filter {
+    (PasswordLastSet -gt $expiredDate) -and
+    (PasswordNeverExpires -eq $false) -and
+    (Enabled -eq $true)
+} -Properties PasswordNeverExpires, PasswordLastSet, Mail |
+  select samaccountname, PasswordLastSet, Mail,
+  @{
+    name = "DaysUntilExpired";
+    Expression = {$_.PasswordLastSet - $ExpiredDate | select -ExpandProperty Days}
+  } |
+  Sort-Object PasswordLastSet
+
+$ExpiredUsers = Get-ADUser -Filter {
+    (PasswordLastSet -lt $expiredDate) -and
+    (PasswordNeverExpires -eq $false) -and
+    (Enabled -eq $true)
+} -Properties PasswordNeverExpires, PasswordLastSet, Mail |
+  select samaccountname, PasswordLastSet, Mail,
+  @{
+    name = "DaysUntilExpired";
+    Expression = {$_.PasswordLastSet - $ExpiredDate | select -ExpandProperty Days}
+  } |
+  Sort-Object PasswordLastSet
+
+$AllUsers = $NonExpiredUsers + $ExpiredUsers
+
+$AllUsers | ConvertTo-Json
